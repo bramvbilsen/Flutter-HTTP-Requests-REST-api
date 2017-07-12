ee40e132e1e80038b8412daf4400012b7204548a
Bram Vanbilsen <bramvbilsen@gmail.com>
2017-07-12 14:04:27 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+      headers: {
+        "Accept": "application/json"
+      }
+    );
+    data = JSON.decode(response.body);
+    print(data[1]["title"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Get data"),
+          onPressed: getData,
+        ),
+      ),
+    );
+  }
+}
